69f3fab0fdb880a9a239655db13f74bddd7c0faa
Ayman Al-Shraiedeh <aymanshraideh96@gmail.com>
2022-07-29 21:47:10 +0300

    main

---
+++ dashboard/src/App.js
@@ -0,0 +1,49 @@
+
+import Sidebar from "./components/layout/sidebar";
+import Footer from "./components/layout/footer";
+import Home from "./components/home";
+import {BrowserRouter , Route , Routes} from 'react-router-dom'
+import Products from "./components/products";
+import Users from "./components/users";
+import Business from "./components/business";
+function App() {
+  return (
+    <>
+    
+    
+   
+    <BrowserRouter>
+    <Sidebar />
+
+    <Routes >
+    {/* this is Home */}
+    <Route path="/" element={<Home />} />
+
+    {/* this is Bussines form for add */}
+    {/* <Route path="add" element={<Bussines_addForm />}/> */}
+
+      <Route  />
+      <Route path="/products" element={<Products />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="/business" element={<Business />} />
+
+      {/* <Route path="contact" element={<Contact />}/>
+      <Route path="about" element={<About />}/>
+      <Route path="login" element={<Login />}/>
+      <Route path="register" element={<Register />}/>
+      <Route path="bussnisRegister" element={<BussnisRegister />}/>
+      <Route path="business" element={<Business />}/>
+
+      <Route path="userprofile" element={<UserProfile />}/>
+
+      <Route path="business_profile" element={<Business_profile />}/>
+      <Route path="products" element={<Products />}/> */}
+    </Routes>
+
+    <Footer />
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
