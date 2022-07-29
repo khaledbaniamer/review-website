25671b2726e4b3c8f0621d06140dcc04ba11df25
khaled94 <khaledbaniamer23@gmail.com>
2022-07-29 19:22:37 +0200

    main

--- front-end/src/components/Bussines_addForm.js
+++ front-end/src/components/Bussines_addForm.js
@@ -107,48 +107,7 @@ function Bussines_addForm() {
                 </form>
               </div>
 
-              {/* <div class="col-lg-4">
-                <div class="media contact-info">
-                  <span class="contact-info__icon">
-                    <i class="ti-home"></i>
-                  </span>
-                  <div class="media-body">
-                    <h3>Jordan, Irbid.</h3>
-                    <p>0096277809091</p>
-                  </div>
-                </div>
-                <div class="media contact-info">
-                  <span class="contact-info__icon">
-                    <i class="ti-tablet"></i>
-                  </span>
-                  <div class="media-body">
-                    <h3>
-                      <a href="tel:454545654">00 (440) 9865 562</a>
-                    </h3>
-                    <p>Mon to Fri 9am to 6pm</p>
-                  </div>
-                </div>
-                <div class="media contact-info">
-                  <span class="contact-info__icon">
-                    <i class="ti-email"></i>
-                  </span>
-                  <div class="media-body">
-                    <h3>
-                      <a href="mailto:support@colorlib.com">
-                        support@colorlib.com
-                      </a>
-                    </h3>
-                    <p>Send us your query anytime!</p>
-                  </div>
-                </div>
-                <a
-                  href="mailto:someone@example.com"
-                  type="submit"
-                  class="main_btn"
-                >
-                  Direct Email
-                </a> */}
-              {/* </div> */}
+
             </div>
           </div>
         </div>
