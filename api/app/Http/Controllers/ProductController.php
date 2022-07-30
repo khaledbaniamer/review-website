167ec915d3d6bfe5d6c51f30d52e930ca21c4974
qutaibahkhasawneh <100359595+qutaibahkhasawneh@users.noreply.github.com>
2022-07-30 21:27:25 +0300

    product

--- api/app/Http/Controllers/ProductController.php
+++ api/app/Http/Controllers/ProductController.php
@@ -5,30 +5,55 @@ namespace App\Http\Controllers;
 use App\Models\Business;
 use Illuminate\Http\Request;
 use App\Models\Product;
-
+use Illuminate\Support\Facades\Validator;
 class ProductController extends Controller
 {
     public function ownerAddProduct(Request $request){
-        // $request->validate([
-        //     'product_name'=>'required',
-        //     'product_description'=>'required',
-        //     'product_image'=>'required',
-        //     'category_id'=>'required'
 
-
-
-        // ]);
-        $imageName = time().'-'.'-'.$request->file('product_image')->extension();
-        $request->file('product_image')->move(public_path('PostsImage'), $imageName);
-        $product = Product::create([
-        'product_name'=>$request->product_name,
-        'product_description'=>$request->product_description,
-        'product_image'=>$imageName,
-        'catrgory_id'=>$request->category_id,
-        'business_id'=>$request->business_id
+        
+        $validator = Validator::make($request->all(),[
+            // 'credential_photo'=>'required|image|max:2048',
+            // 'profile_photo'=>'required|image|max:2048',
+            'product_name'=>'required',
+            'product_description'=>'required',
+            'catrgory_id'=>'required',
+            'business_id'=>'required',
+            'product_image'=>'required|image|max:2048',
+            'product_image1'=>'required|image|max:2048',
+            'product_image2'=>'required|image|max:2048',
 
     ]);
-    return $product;
+
+    if($validator->fails()){
+        return response()->json(['errors'=>$validator->errors(),'status'=>400]);
+    }
+        $product = new Product();
+        $product->product_name = $request->product_name;
+        $product->product_description = $request->product_description;
+        $product->catrgory_id = $request->catrgory_id;
+        $product->business_id = $request->business_id;
+
+
+        if($request->has('product_image')) {
+            $image= $request->file('product_image');
+            $filename =time().'.'.$image->getClientOriginalExtension();
+            $image->move('product_image', $filename);
+            $product->product_image = $filename;
+            }
+        if($request->has('product_image1')) {
+            $image= $request->file('product_image1');
+            $filename =time().'.'.$image->getClientOriginalExtension();
+            $image->move('product_image', $filename);
+            $product->product_image1 = $filename;
+            }
+        if($request->has('product_image2')) {
+            $image= $request->file('product_image2');
+            $filename =time().'.'.$image->getClientOriginalExtension();
+            $image->move('product_image', $filename);
+            $product->product_image2 = $filename;
+            }
+        $product->save();
+        return $product;
     }
 
     public function products_business($id)
@@ -37,7 +62,7 @@ class ProductController extends Controller
         // ->join('managers', 'managers.id', '=', 'associations.assoc_manager_id')->get();
         $products = Product::select('products.*', 'businesses.*' ,'categories.category_name')->join('businesses','businesses.id' , '=' ,'products.business_id')->join('categories','categories.id' , '=' ,'products.catrgory_id')->where('businesses.id' , $id)->get();
 
-        
+
         return $products;
     }
 }
