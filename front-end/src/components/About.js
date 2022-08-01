d7b9572f80d63456b014b06d1d3e93cb87ce253d
Momen-Alshouha <momenalshouha@outlook.com>
2022-08-01 11:13:30 +0300

    a

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -25,7 +25,7 @@ class About extends React.Component {
               <div className="container py-5">
                 <div className="row h-100 align-items-center py-5">
                   <div className="col-lg-6">
-                    <h1 className="display-4">About us page</h1>
+                    <h1 className="display-4">About us</h1>
                     <p className="lead text-muted mb-0">Create a minimal about us page using Bootstrap 4.</p>
                     <p className="lead text-muted">Snippet by <a href="https://bootstrapious.com/snippets" class="text-muted"> 
                                 <u>Bootstrapious</u></a>
@@ -41,37 +41,37 @@ class About extends React.Component {
         <div className="row mb-4">
           <div className=" col-lg-5">
             <h2 className="display-4 font-weight-light">Our team</h2>
-            <p className="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <p className="font-italic text-muted">Meer Our Team.</p>
           </div>
         </div>
 
         <div className="row text-center">
           <div className="col-xl-12 col-sm-6 mb-5">
-            <div className="bg-light rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-              <h5 className="mb-0">Manuella Nevoresky</h5><span className="small text-uppercase text-muted">CEO - Founder</span>
+            <div className="bg-light rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+              <h5 className="mb-0">Qutaibah khasawaneh</h5><span className="small text-uppercase text-muted">Product Owner</span>
             </div>
           </div>
           
           <div className="col-xl-3 col-sm-6 mb-5">
             <div className="bg-light rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-              <h5 className="mb-0">Samuel Hardy</h5><span className="small text-uppercase text-muted">CEO - Founder</span>
+              <h5 className="mb-0">Yousef Alhindawi</h5><span className="small text-uppercase text-muted">Scrum Master</span>
             </div>
           </div>
         
         <div className="col-xl-3 col-sm-6 mb-5">
           <div className="bg-light rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-            <h5 className="mb-0">Tom Sunderland</h5><span className="small text-uppercase text-muted">CEO - Founder</span>
+            <h5 className="mb-0">Ayman shraideh</h5><span className="small text-uppercase text-muted">Developer</span>
           </div>
         </div>
 
               <div className="col-xl-3 col-sm-6 mb-5">
                   <div className="bg-light rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                  <h5 className="mb-0">John Tarly</h5><span className="small text-uppercase text-muted">CEO - Founder</span>
+                  <h5 className="mb-0">Khaled Bani Amer</h5><span className="small text-uppercase text-muted">Developer</span>
                   </div>
               </div>
               <div className="col-xl-3 col-sm-6 mb-5">
                   <div className="bg-light rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                  <h5 className="mb-0">John Tarly</h5><span className="small text-uppercase text-muted">CEO - Founder</span>
+                  <h5 className="mb-0">Mo'men Alshouha </h5><span className="small text-uppercase text-muted">Developer</span>
                   </div>
               </div>
 
